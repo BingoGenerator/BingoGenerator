5411e53b8261a4c939e8b9152155a48dce47a6cb
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-01-05 22:06:49 +0100

    remove old board type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectionComponent } from './selection/selection.component';
-import { BoardComponent } from './board/board.component';
 import { DetailBoardComponent } from './detail-board/detail-board.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         SelectionComponent,
-        BoardComponent,
         DetailBoardComponent
     ],
     imports: [
